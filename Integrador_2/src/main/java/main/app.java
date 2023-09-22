9870afb26781794c18bca2e5d15550e5a1913f19
M Victoria <mvittoria7@gmail.com>
2023-09-22 10:45:55 -0300

    Ejercicio3, terminado

--- Integrador_2/src/main/java/main/app.java
+++ Integrador_2/src/main/java/main/app.java
@@ -8,6 +8,7 @@ import entity.Carrera_inscripta;
 import repository.AlumnoRepositoryImpl;
 import service.CarreraService;
 
+import java.util.Date;
 import java.util.List;
 
 public class app {
@@ -15,44 +16,43 @@ public class app {
         RepositoryFactory.getInstance(RepositoryFactory.MYSQL);
 
         /******PRUEBAS**********/
-        /**creo un alumno**
-       // Alumno a= new Alumno("ff","agga",21,'m',25502633,"Tres Arroyos",2553154,null);
-        /**creo una carrera**
+        /**creo un alumno**/
+       Alumno a= new Alumno("ff","agga",21,'m',25502633,"Tres Arroyos",2553154,null);
+        /**creo una carrera**/
         Carrera carrera = new Carrera("Abogacia",null);
         CarreraRepositoryImpl carreraRepository = new CarreraRepositoryImpl();
         carreraRepository.insert(carrera);
-        /**busco una carrera por la id**
+        /**busco una carrera por la id**/
         Carrera carrera1;
         carrera1=carreraRepository.findById(1);
-        /**inserto el alumno nuevo a la db**
+        /**inserto el alumno nuevo a la db**/
        AlumnoRepositoryImpl alumno= new AlumnoRepositoryImpl();
         alumno.insert(a);
-        /**busco el alumno**
+        /**busco el alumno**/
         Alumno encontrado;
         encontrado=alumno.findById(1);
-        /**creo una carrera inscripta para el alumno que busque y con la carrera buscada**
+        /**creo una carrera inscripta para el alumno que busque y con la carrera buscada**/
         Carrera_inscripta ca = new Carrera_inscripta(0,false,carrera1,encontrado);
         CarreraInscriptaRepositoryImpl carreraInscripta = new CarreraInscriptaRepositoryImpl();
-        /**inserto la carrera inscripta en la db**
+        /**inserto la carrera inscripta en la db**/
         carreraInscripta.insert(ca);
 
-        System.out.println(encontrado.toString());*/
+        System.out.println(encontrado.toString());
+
         //Obtengo el respositorio
         AlumnoRepository alumnoRepository = RepositoryFactory.get_repositorio_alumno();
-        CarreraRepository carreraRepository = RepositoryFactory.get_repositorio_carrera();
-        CarreraInscriptaRepository carreraInscriptaRepository = RepositoryFactory.get_repositorio_carreraInscripta();
-
+        //servicios
         CarreraService carreraService = new CarreraService(RepositoryFactory.getEntity_manager());
 
         //e) recuperar todos los estudiantes, en base a su género
 
-       /*List<Alumno> busquedaPorGenero = alumnoRepository.findAllByGender('M');
-       for(Alumno a: busquedaPorGenero){
-           System.out.println(a.getId()+ " | "+a.getNombre()+" | "+a.getApellido()+" | "+a.getGenero());
-       }*/
+       List<Alumno> busquedaPorGenero = alumnoRepository.findAllByGender('M');
+       for(Alumno alu: busquedaPorGenero){
+           System.out.println(alu.getId()+ " | "+alu.getNombre()+" | "+alu.getApellido()+" | "+alu.getGenero());
+       }
 
        //f) recuperar las carreras con estudiantes inscriptos, y ordenar por cantidad de inscriptos.
-        /*List<Object[]>resultados= carreraService.obtenerCantidadInscriptoPorCarrera();
+        List<Object[]>resultados= carreraService.obtenerCantidadInscriptoPorCarrera();
         System.out.println("Carreras con estudiantes inscriptos ordenados por cantidad de inscriptos");
         for(Object[] resultado : resultados){
             String nombreCarrera = (String) resultado[0];
@@ -60,21 +60,47 @@ public class app {
             System.out.println("Carrera: "+ nombreCarrera);
             System.out.println("Inscriptos: "+cantidadInscriptos);
             System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-        }*/
+        }
         //g) recuperar los estudiantes de una determinada carrera, filtrado por ciudad de residencia.
-        List<Object[]>resultadoBusquedad = carreraService.obtenerEstudiantePorCarreraYCiudad("Abogacia", "Tres Arroyos");
+       List<Object[]>resultadoBusquedad = carreraService.obtenerEstudiantePorCarreraYCiudad("Abogacia", "Tres Arroyos");
         System.out.println("Listado de estudiantes filtrado por carrera y residencia");
         for(Object[] datoEncontrado : resultadoBusquedad){
             String nombre = (String) datoEncontrado[0];
             String apellido = (String) datoEncontrado[1];
-            String carrera = (String) datoEncontrado[2];
+            String nombcarrera = (String) datoEncontrado[2];
             String ciudad = (String) datoEncontrado[3];
             System.out.println("Nombre: "+nombre);
             System.out.println("Apellido: "+apellido);
-            System.out.println("Carrera: "+carrera);
+            System.out.println("Carrera: "+nombcarrera);
             System.out.println("Ciudad: "+ciudad);
             System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
         }
+
+        /*Ejercicio 3:
+        * Generar un reporte de las carreras, que para cada carrera incluya información de los
+        * inscriptos y egresados por año. Se deben ordenar las carreras alfabéticamente, y presentar
+        * los años de manera cronológica
+        */
+        List<Object[]>reporte = carreraService.obtenerReporteDeCarreras();
+        for(Object[] data: reporte){
+            int libreta = (int) data[0];
+            String nombreAlumno = (String) data[1];
+            String apellidoAlumno = (String) data[2];
+            String nombreCarrera =(String) data[3];
+            Date graduado = (Date) data[4];
+            Long inscriptos =(Long) data[5];
+            Long egresado =(Long) data[6];
+
+            System.out.println("N° libreta: "+libreta);
+            System.out.println("Nombre: "+nombreAlumno);
+            System.out.println("Apellido: "+apellidoAlumno);
+            System.out.println("Carrera: "+nombreCarrera);
+            System.out.println("Año de graduado: "+graduado);
+            System.out.println("Inscripto: "+inscriptos);
+            System.out.println("Egresado: "+egresado);
+            System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        }
+
 
 
     }
--- Integrador_2/src/main/java/service/CarreraService.java
+++ Integrador_2/src/main/java/service/CarreraService.java
@@ -28,11 +28,33 @@ public class CarreraService {
                 "FROM Carrera_inscripta ci " +
                 "JOIN ci.alumno a " + 
                 "JOIN ci.carrera c " + 
-                "WHERE c.nombre = :nombreCarrera " +
+                "WHERE c.nombre = :nombreCarrera "  +
                 "AND a.residencia = :ciudadResidencia", Object[].class);
         query.setParameter("nombreCarrera",nombreCarrera);
         query.setParameter("ciudadResidencia", ciudadResidencia);
 
+        return query.getResultList();
+    }
+
+    /*Ejercicio 3:
+    * Generar un reporte de las carreras, que para cada carrera incluya información de los
+    * inscriptos y egresados por año. Se deben ordenar las carreras alfabéticamente, y presentar
+    * los años de manera cronológica
+    */
+    public List<Object[]> obtenerReporteDeCarreras() {
+        TypedQuery<Object[]>query = entityManager.createQuery(
+                "SELECT a.num_libreta_un AS libreta," +
+                    "a.nombre AS nombreAlumno," +
+                    "a.apellido AS apellidoAlumno," +
+                    "c.nombre AS nombreCarrera," +
+                    "ci.anio_graduacion AS añoGraduado," +
+                    "COUNT(DISTINCT ci) AS inscriptos," +
+                    "SUM(CASE WHEN ci.graduado = true THEN 1 ELSE 0 END) AS egresados " +
+                    "FROM Carrera_inscripta ci " +
+                    "LEFT JOIN ci.alumno a " +
+                    "LEFT JOIN ci.carrera c " +
+                    "GROUP BY c.nombre, ci.anio_graduacion, a.nombre, a.apellido " +
+                    "ORDER BY c.nombre ASC, ci.anio_graduacion ASC",Object[].class);
         return query.getResultList();
     }
 }
